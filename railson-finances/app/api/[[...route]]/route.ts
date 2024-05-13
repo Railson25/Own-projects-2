d3de47c4abb66b1056b02e8a7bab909d42a70790
Railson25 <railsonsantiago123@gmail.com>
2024-05-13 15:54:23 -0300

    feat(api get): Accounts
    This commit will create a useGetAccouts and will handle errors correctly
    in our api also will using reactquery for mutations loading erros and
    more

--- railson-finances/app/api/[[...route]]/route.ts
+++ railson-finances/app/api/[[...route]]/route.ts
@@ -6,9 +6,17 @@ export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
+// app.onError((err, c) => {
+//   if (err instanceof HTTPException) {
+//     return err.getResponse();
+//   }
+
+//   return c.json({ error: "Internal server error" }, 500);
+// });
+
 const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
 
-export type Apptype = typeof routes;
+export type AppType = typeof routes;
