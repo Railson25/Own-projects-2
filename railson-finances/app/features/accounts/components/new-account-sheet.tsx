f88ca77f6b60455cb706a779631f230a389a482d
Railson25 <railsonsantiago123@gmail.com>
2024-05-14 14:02:06 -0300

    feat(form): Account
    This commit will create account form using shadcn component

---
+++ railson-finances/app/features/accounts/components/new-account-sheet.tsx
@@ -0,0 +1,45 @@
+import { z } from "zod";
+import { insertAccountSchema } from "@/db/schema";
+import { useNewAccount } from "../hooks/use-new-accounts";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { AccountForm } from "./account-form";
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+export const NewAccountSheet = () => {
+  const { isOpen, onClose } = useNewAccount();
+
+  const onSubmit = (values: FormValues) => {
+    console.log({ values });
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className="space-y-4">
+        <SheetHeader>
+          <SheetTitle>New Account</SheetTitle>
+          <SheetDescription>
+            Create a new account to track your transactions.
+          </SheetDescription>
+        </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={false}
+          defaultValues={{
+            name: "",
+          }}
+        />
+      </SheetContent>
+    </Sheet>
+  );
+};
