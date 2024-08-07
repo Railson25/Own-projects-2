060490c815ff61c1a25911d1323c359ea2029875
Railson25 <railsonsantiago123@gmail.com>
2024-08-07 10:19:30 -0300

    feat(setup): project
    This commit will create and config the care pulse project

---
+++ care-pulse/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-500">wegtwertg</h1>
+      <Button>click</Button>
+    </div>
+  );
+}
