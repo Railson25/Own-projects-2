ef12ad26c3b35271bf08043a471770db6369371b
Railson25 <railsonsantiago123@gmail.com>
2025-04-19 19:28:55 -0300

    feat(data-structure): update pointers implementation and add rectangle struct
    - Added a new struct `rectangle` to define length and breadth.
    - Modified the `main` function to include multiple pointer types: `int`, `char`, `float`, `double`, and `rectangle`.
    - Removed the previous pointer implementation and associated memory allocation code.
    - Added size output for each pointer type to demonstrate their sizes.
    
    These changes enhance the pointers implementation by introducing a structured approach to handle rectangle dimensions and improving the understanding of pointer sizes in C++. This refactor sets a foundation for future enhancements related to data structures.

--- data-structure/pointers.cpp
+++ data-structure/pointers.cpp
@@ -5,22 +5,47 @@
 using namespace std;
 
 
+// int main(){
+   
+//     int *p;
+//     // p=(int *)malloc(5 * sizeof(int));
+//     p = new int[5];
+//     p[0] = 10;
+//     p[1] = 18;
+//     p[2] = 9;
+//     p[3] = 21;
+//     p[4] = 50;
+
+//     for(int i = 0; i < 5;i++)
+//     cout << p[i] << endl;
+
+//     delete [ ] p;
+//     return 0;
+// }
 
 
+struct rectangle
+{
+    int length;
+    int breadth;
+};
+
+
+// Pointer takes 8bytes independent of the data in latest compiler they take 8 bytes! in earlier they take 4bytes.
 int main(){
    
-    int *p;
-    // p=(int *)malloc(5 * sizeof(int));
-    p = new int[5];
-    p[0] = 10;
-    p[1] = 18;
-    p[2] = 9;
-    p[3] = 21;
-    p[4] = 50;
+    int *p1;
+    char *p2;
+    float *p3;
+    double *p4;
+    struct rectangle *p5;
 
-    for(int i = 0; i < 5;i++)
-    cout << p[i] << endl;
+    cout << sizeof(p1) << endl;
+    cout << sizeof(p2) << endl;
+    cout << sizeof(p3) << endl;
+    cout << sizeof(p4) << endl;
+    cout << sizeof(p5) << endl;
 
-    delete [ ] p;
+
     return 0;
 }
