91a36c68936f1428ed5698b5d96a5a8fd9375eed
Railson25 <railsonsantiago123@gmail.com>
2025-04-20 11:40:52 -0300

    feat(data-structure): update struct parameter handling in C++
    - Commented out the previous implementation of passing struct by value in the function `fun`.
    - Updated the `fun` function to accept a pointer to a struct rectangle, allowing modification of the original struct.
    - Adjusted the `main` function to pass the address of the struct to `fun` instead of passing by value.
    
    These changes enhance the functionality by demonstrating how to pass structs by reference, which allows for direct modification of the struct's properties, improving memory efficiency and performance.

--- data-structure/C-C++/struct-as-parameter.cpp
+++ data-structure/C-C++/struct-as-parameter.cpp
@@ -10,17 +10,34 @@ struct rectangle
 };
 
 
-void fun(struct rectangle r1){
-    r1.length = 45;
-    cout << r1.length << endl;
-    cout << r1.breadth << endl;
+// void fun(struct rectangle r1){
+//     r1.length = 45;
+//     cout << r1.length << endl;
+//     cout << r1.breadth << endl;
+// }
+
+// // Passing struct parameter as a value 
+// int main(){
+
+//     struct rectangle r= {10, 5};
+//     fun(r);
+//     printf("%d\n", r.length, r.breadth);
+
+//     return 0;
+// }
+
+
+void fun(struct rectangle *p){
+    p -> length = 45;
+    cout << p -> length << endl;
+    cout << p -> breadth << endl;
 }
 
-// Passing struct parameter as a value 
+// Passing struct parameter as a address 
 int main(){
 
     struct rectangle r= {10, 5};
-    fun(r);
+    fun(&r);
     printf("%d\n", r.length, r.breadth);
 
     return 0;
