cb25c864617bd5156bacdd938c99102c880f4e38
Railson25 <railsonsantiago123@gmail.com>
2025-04-20 11:06:33 -0300

    feat(data-structure): refactor array handling in function
    - Changed the function signature from void to return a pointer to an integer array.
    - Removed the parameter for the array and its size, creating a new array in heap memory instead.
    - Updated the main function to call the modified function and print the returned array.
    
    This refactor improves the function's usability by allowing it to dynamically create and return an array, enhancing memory management and flexibility in handling array sizes.

--- data-structure/array-as-parameter.cpp
+++ data-structure/array-as-parameter.cpp
@@ -4,18 +4,25 @@ using namespace std;
 
 
 //Arrays are always passed by address.
-void fun(int *a, int n){
+int * fun(int size){
+    int * p;
+    // Create an array in heap memory
+    p = new int[size];
 
-    a[0] = 28;
+    for(int i = 0; i < size; i++)
+    p[i] = i + 1;
+
+    // returning a pointer
+    return p;
 }
 
 int main(){
-    int a[] = {2,4,6,8,10};
-    int n = 5;
+    int *ptr, sz = 5;
 
-    fun(a, n);
-    for(int x:a)
-    cout << x << endl;
+    // This function will create an array and returning the pointer for that array
+    ptr = fun(sz);
+    for(int i = 0; i < sz; i++)
+    cout << ptr[i] << endl;
 
     return 0;
 }
