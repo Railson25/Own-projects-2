2ee21c56e5642254b2de7d6c212d14800b36d5f7
Railson25 <railsonsantiago123@gmail.com>
2025-04-19 21:03:50 -0300

    feat(data-structure): update functions for parameter passing and executable
    - Commented out the 'add' function and its usage in 'main' to focus on pointer-based parameter passing.
    - Enhanced the 'swap' function to demonstrate call by address with pointers.
    - Updated 'main' to reflect changes, showcasing the swapping of values between 'num1' and 'num2'.
    - Removed unused variable 'sum' for clarity.
    
    These changes improve the clarity of parameter passing techniques in C++, emphasizing the use of pointers for modifying values directly. This enhances the educational value of the code by providing a clearer example of call by address.

--- data-structure/functions-passing-parameters.cpp
+++ data-structure/functions-passing-parameters.cpp
@@ -6,19 +6,40 @@ using namespace std;
 //Call by value parameters!
 //Every changes that happened  with (a,b) variables will not
 //change the values of (num1, num2) variables.
-int add(int a, int b){
-    a++;
-     cout << a << endl;
-    return 0;
+// int add(int a, int b){
+//     a++;
+//      cout << a << endl;
+//     return 0;
+// }
+
+// int main(){
+//     int num1 = 10, num2 = 15, sum;
+
+//     sum = add(num1, num2);
+
+//     cout << num1;
+
+//     return 0;
+// }
+
+
+//Call by address parameters!
+//Parameters is type of Pointers.
+void swap(int *a, int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int main(){
-    int num1 = 10, num2 = 15, sum;
+    int num1 = 10, num2 = 15;
 
-    sum = add(num1, num2);
+    //Passing the address of parameters
+    swap(&num1, &num2);
 
-    cout << num1;
+    cout << num1 << endl;
+    cout << num2 << endl;
 
     return 0;
 }
-
