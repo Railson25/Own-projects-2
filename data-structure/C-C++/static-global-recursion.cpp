ad145027239303673bb53be882157f9e59a687e0
Railson25 <railsonsantiago123@gmail.com>
2025-06-27 15:35:39 -0300

    feat(data-structure): add static global recursion example
    - Created a new C++ file for demonstrating static global recursion.
    - Implemented a recursive function that utilizes a static global variable.
    - Added a main function to test the recursive function with sample input.
    
    This addition provides a practical example of using static variables in recursion, enhancing the educational resources in the data-structure module.

---
+++ data-structure/C-C++/static-global-recursion.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+static int x = 0; // using global variable
+
+int fun(int n) {
+
+    // static int x = 0; using scope variable
+    if(n > 0) {
+        x++;
+        return fun(n - 1) + x;
+    }
+
+    return 0;
+}
+
+
+int main(){
+    int r;
+  
+    r = fun(5);
+    printf("%d\n", r);
+
+    r = fun(5);
+    printf("%d\n", r);
+
+    return 0;
+}
