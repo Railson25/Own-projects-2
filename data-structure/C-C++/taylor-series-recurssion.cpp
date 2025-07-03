c5a293ec77ed431f10c29704d66619af5d350682
Railson25 <railsonsantiago123@gmail.com>
2025-07-02 22:30:12 -0300

    feat(data-structure): add Taylor series recursion examples in C++
    - Implemented three functions to calculate the Taylor series:
      - `e(int x, int n)` for O(n^2) complexity.
      - `eHorner(int x, int n)` for O(n) complexity using Horner's method.
      - `Ie(int x, int n)` using a loop for iterative calculation.
    - Added a main function to demonstrate the usage of the `Ie` function.
    
    These additions provide multiple approaches to compute the Taylor series, enhancing the educational value of the codebase and offering users various methods to choose from based on their performance needs.

---
+++ data-structure/C-C++/taylor-series-recurssion.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+// O(n square)
+double e(int x, int n){
+    static double p = 1, f = 1;
+    double r;
+    if(n == 0)
+        return 1;
+    r = e(x, n - 1);    
+    p = p * x;  
+    f = f * n;
+
+    return r + p / f ;
+
+}
+
+// Making faster O(n)
+double eHorner(int x , int n){
+    static double s;
+
+    if(n == 0)
+        return s;
+    s = 1 + x * s / n;   
+    return eHorner(x, n - 1);   
+}
+
+//Using loop
+double Ie(int x, int n){
+    double s = 1;
+    int i;
+    double num = 1, den = 1;
+
+    for(i = 1; i <= n; i ++){
+        num *= x;
+        den *= i;
+        s += num/den;
+    }
+
+    return s;
+}
+
+int main(){
+
+    printf("%lf \n", Ie(1,10));
+    return 0;
+}
