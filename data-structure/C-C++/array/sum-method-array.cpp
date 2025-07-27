f8fdb5f52fe3902958357aceaffa6ba10d645e86
Railson25 <railsonsantiago123@gmail.com>
2025-07-27 15:48:44 -0300

    array functions

---
+++ data-structure/C-C++/array/sum-method-array.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Array{
+    int a[10];
+    int size;
+    int length;
+};
+
+int Sum(struct Array arr){
+    int s = 0;
+    int i;
+    for(i = 0; i < arr.length; i++)
+       s = s + arr.a[i]; 
+    return s;   
+}
+
+int main(){
+    struct Array arr={{23,3,14,25,6}, 10, 5};
+    printf("%d\n", Sum(arr));
+
+    return 0;
+}
