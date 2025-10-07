4df6227657b182e01e9814a123affaf93ae4e24c
Railson25 <railsonsantiago123@gmail.com>
2025-10-07 11:28:32 -0300

    feat(array): implement functionality to find pair elements with a given sum in a sorted array - Added a new program to find pairs in a sorted array that sum up to a specified target value. - Implemented a two-pointer approach to efficiently find and print the pairs. - Created a new executable file for the implemented functionality.
    This addition enhances the array manipulation capabilities of the project, providing a useful tool for users needing to identify pairs that meet specific criteria.

---
+++ data-structure/C-C++/string/changing-case.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    char A[] = "wElCome"; 
+    int i;
+
+    for(i = 0; A[i] != '\0'; i++){
+        if(A[i] >= 'A' && A[i] <= 'Z'){
+            A[i] += 32;
+        }else if(A[i] >= 'a' && A[i] <= 'z'){
+            A[i] -= 32;
+        }
+    }
+
+   printf("%s", A);
+
+    return 0;
+}
